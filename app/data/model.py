38999df69de02e9106745f6c26d7f9da282c503f
Paul Aigbokhai Olukayode <paulaigbokhaiolukayode@USERs-MacBook-Pro.local>
2024-02-20 16:42:58 +0100

    Initial commit

--- app/data/model.py
+++ app/data/model.py
@@ -1,8 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 
 class Continent(BaseModel):
     id: int
     name: str
     created_at: datetime
+    
+class Employee(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+    
